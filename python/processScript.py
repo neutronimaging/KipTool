18481fb2b99fe1432c327606a1e92db54a9f2ecd
Anders Kaestner <anders.kaestner@psi.ch>
2020-07-09 16:08:15 +0200

    Fixed some errors

--- python/processScript.py
+++ python/processScript.py
@@ -4,7 +4,8 @@ from subprocess import call
 from math import fmod
 
 srcmask="D:/P20170229_Snehota/04_evaluation/GR2_S2_6/recon/frame_{0:04}.tif"
-dstmask="D:/P20170229_Snehota/04_evaluation/GR2_S2_6/filtered/fframe_{0:04}.tif"
+dstpath="outimage:dstpath=D:/P20170229_Snehota/04_evaluation/GR2_S2_6/filtered/"
+dstmask="fframe_{0:04}.tif"
 
 # path to the application
 kiptool="C:/Users/ander/source/repos/deployed/kiptool/KipToolCLI.exe"
@@ -20,4 +21,4 @@ for i in range(firstframe,lastframe) :
     # adjust the reconstruction angles to alternating between 0-180 and 180-360
     # call processing
 
-    call([kiptool, "-f", cfgpath, srcname, dstname])
+    call([kiptool, "-f", cfgpath, srcname, dstpath, dstname])
