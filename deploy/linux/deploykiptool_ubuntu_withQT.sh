60b1e15ae86d8566bbcd6e322501a5f74c99f0fd
Anders Kaestner <anders.kaestner@psi.ch>
2022-07-07 10:14:07 +0200

    kiptool runs with qt6 and cmake on ubuntu.

--- deploy/linux/deploykiptool_ubuntu_withQT.sh
+++ deploy/linux/deploykiptool_ubuntu_withQT.sh
@@ -2,11 +2,10 @@
 #CPCMD="ln -s "
 CPCMD="cp "
 REPOSPATH=$WORKSPACE
-QT_PATH=$QTPATH
+QT_PATH=$QTBINPATH/..
 #declare -i QtV=${QT_PATH:end-11:end-8}
-QtV=${QT_PATH:end-11:end-7}
-QtVmain=${QT_PATH:end-11:end-9}
-
+QtV=${QT_PATH:end-17:end-14}
+QtVmain=${QT_PATH:end-17:end-16}
 
 echo $QtV
 echo $QtVmain
@@ -14,7 +13,6 @@ echo $QT_PATH
 
 
 DEST="$REPOSPATH/deploy/kiptool"
-#DEST="$HOME/kiptool"
 
 mkdir --parent $DEST
 mkdir --parent $DEST/bin
@@ -25,65 +23,79 @@ pushd .
 cd $DEST/Frameworks
 rm -f *.so.1.0.0
 
-`$CPCMD $REPOSPATH/lib/libQtAddons.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libQtModuleConfigure.so.1.0.0 .`
+`$CPCMD $REPOSPATH/install/lib/libQtAddons.so .`
+`$CPCMD $REPOSPATH/install/lib/libQtModuleConfigure.so .`
 
-`$CPCMD $REPOSPATH/lib/libImagingAlgorithms.so.1.0.0 .`
+`$CPCMD $REPOSPATH/install/lib/libImagingAlgorithms.so .`
+`$CPCMD $REPOSPATH/install/lib/libAdvancedFilters.so .`
+`$CPCMD $REPOSPATH/install/lib/libkipl.so .`
+`$CPCMD $REPOSPATH/install/lib/libModuleConfig.so .`
+`$CPCMD $REPOSPATH/install/lib/libProcessFramework.so .`
 
-`$CPCMD $REPOSPATH/lib/libkipl.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libModuleConfig.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libProcessFramework.so.1.0.0 .`
-
-`$CPCMD $REPOSPATH/lib/libReaderConfig.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libReaderGUI.so.1.0.0 .`
+`$CPCMD $REPOSPATH/install/lib/libReaderConfig.so .`
+`$CPCMD $REPOSPATH/install/lib/libReaderGUI.so .`
 
 # Module libs
-`$CPCMD $REPOSPATH/lib/libAdvancedFilterModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libAdvancedFilterModulesGUI.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libBaseModules.so.1.0.0  .`
-`$CPCMD $REPOSPATH/lib/libBaseModulesGUI.so.1.0.0  .`
+`$CPCMD $REPOSPATH/install/lib/libAdvancedFilterModules.so .`
+`$CPCMD $REPOSPATH/install/lib/libAdvancedFilterModulesGUI.so .`
+`$CPCMD $REPOSPATH/install/lib/libBaseModules.so .`
+`$CPCMD $REPOSPATH/install/lib/libBaseModulesGUI.so  .`
 #`$CPCMD $REPOSPATH/lib/libPorespaceModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libClassificationModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libClassificationModulesGUI.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libImagingModules.so.1.0.0 .`
+`$CPCMD $REPOSPATH/install/lib/libClassificationModules.so .`
+`$CPCMD $REPOSPATH/install/lib/libClassificationModulesGUI.so .`
+`$CPCMD $REPOSPATH/install/lib/libImagingModules.so .`
 #`$CPCMD $REPOSPATH/lib/libPCAModules.so.1.0.0 .`
 #`$CPCMD $REPOSPATH/lib/libStatisticsModules.so.1.0.0 .`
-`$CPCMD $REPOSPATH/lib/libImagingModulesGUI.so.1.0.0 .`
+`$CPCMD $REPOSPATH/install/lib/libImagingModulesGUI.so .`
 
 #qt stuff
-`$CPCMD $QT_PATH/lib/libQt5Core.so.5.$QtV .`
-`$CPCMD $QT_PATH/lib/libQt5Gui.so.5.$QtV .`
-`$CPCMD $QT_PATH/lib/libQt5Widgets.so.5.$QtV .`
-`$CPCMD $QT_PATH/lib/libQt5DBus.so.5.$QtV .`
-`$CPCMD $QT_PATH/lib/libQt5PrintSupport.so.5.$QtV .`
-`$CPCMD $QT_PATH/lib/libQt5XcbQpa.so.5.$QtV .`
-`$CPCMD $QT_PATH/lib/libQt5DBus.so.5.$QtV .`
-`$CPCMD $QT_PATH/lib/libQt5Charts.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libQt5Core.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libQt5Gui.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libQt5Widgets.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libQt5DBus.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libQt5PrintSupport.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libQt5XcbQpa.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libQt5DBus.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libQt5Charts.so.5.$QtV .`
+# `$CPCMD $QT_PATH/lib/libicui18n.so.56.1 .`
+# `$CPCMD $QT_PATH/lib/libicudata.so.56.1 .`
+# `$CPCMD $QT_PATH/lib/libicuuc.so.56.1 .`
+
+`$CPCMD $QT_PATH/lib/libQt6Core.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6Gui.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6Widgets.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6DBus.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6PrintSupport.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6OpenGLWidgets.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6OpenGL.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6XcbQpa.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6DBus.so.6.$QtV .`
+`$CPCMD $QT_PATH/lib/libQt6Charts.so.6.$QtV .`
 `$CPCMD $QT_PATH/lib/libicui18n.so.56.1 .`
 `$CPCMD $QT_PATH/lib/libicudata.so.56.1 .`
 `$CPCMD $QT_PATH/lib/libicuuc.so.56.1 .`
 
 
-rm -f *.so
-rm -f *.so.1
-rm -f *.so.1.0
+# rm -f *.so
+# rm -f *.so.1
+# rm -f *.so.1.0
 
 for f in `ls *.so.1.0.0`; do
 	bn=`basename $f .so.1.0.0`
 	echo $bn
 	ln -s $f $bn.so.1.0
-        ln -s $bn.so.1.0 $bn.so.1
+	ln -s $bn.so.1.0 $bn.so.1
 	ln -s $bn.so.1 $bn.so
 done
 
-rm -f *.so.5.$QtVmain
-rm -f *.so.5
-for f in `ls *.so.5.$QtV`; do
-	bn=`basename $f .so.5.$QtV`
+rm -f *.so.6.$QtVmain
+rm -f *.so.6
+for f in `ls *.so.6.$QtV`; do
+	bn=`basename $f .so.6.$QtV`
 	echo $bn
-	ln -s $f $bn.so.5.$QtVmain
-        ln -s $bn.so.5.$QtVmain $bn.so.5
-	ln -s $bn.so.5 $bn.so
+	ln -s $f $bn.so.6.$QtVmain
+        ln -s $bn.so.6.$QtVmain $bn.so.6
+	ln -s $bn.so.6 $bn.so
 done
 
 rm -f *.so.56
@@ -100,9 +112,9 @@ cd $DEST/bin/platforms
 
 
 cd $DEST/bin
-`$CPCMD $REPOSPATH/Applications/KipTool .`
+`$CPCMD $REPOSPATH/install/applications/KipTool .`
 cd ..
-`$CPCMD $REPOSPATH/imagingsuite/applications/kiptool/scripts/kiptool .`
+`$CPCMD $REPOSPATH/KipTool/applications/kiptool/scripts/kiptool .`
 chmod +x kiptool
 
 fname=kiptool_`uname -s`_`uname -m`_`date +%Y%m%d`.tar.bz2
