e47924065957a89859e844a34d4a78116da2e1f4
Anders Kaestner <anders.kaestner@psi.ch>
2023-01-12 19:43:20 +0100

    First version of find_package build with deploy on windows

--- deploy/win/deploykiptool_win.bat
+++ deploy/win/deploykiptool_win.bat
@@ -5,52 +5,52 @@ IF NOT EXIST %DEST% mkdir %DEST%
 pushd .
 cd %DEST%
 
-copy %REPOS%\build-kiptool\bin\Release\KipTool.exe .
+copy %REPOS%\install\applications\KipTool.exe .
 @REM copy %REPOS%\Applications\KipToolCLI.exe .
-copy %REPOS%\build-imagingsuite\bin\Release\ImagingAlgorithms.dll .
-copy %REPOS%\build-imagingsuite\bin\Release\ModuleConfig.dll .
-copy %REPOS%\build-imagingsuite\bin\Release\ReaderConfig.dll .
-copy %REPOS%\build-imagingsuite\bin\Release\ReaderGUI.dll .
-copy %REPOS%\build-imagingsuite\bin\Release\QtAddons.dll .
-copy %REPOS%\build-imagingsuite\bin\Release\QtModuleConfigure.dll .
-copy %REPOS%\build-imagingsuite\bin\Release\kipl.dll .
-copy %REPOS%\build-kiptool\bin\Release\ProcessFramework.dll .
-copy %REPOS%\build-kiptool\bin\Release\AdvancedFilterModules.dll .
-copy %REPOS%\build-kiptool\bin\Release\AdvancedFilterModulesGUI.dll .
-copy %REPOS%\build-imagingsuite\bin\Release\AdvancedFilters.dll .
-copy %REPOS%\build-kiptool\bin\Release\BaseModules.dll .
-copy %REPOS%\build-kiptool\bin\Release\BaseModulesGUI.dll .
-copy %REPOS%\build-kiptool\bin\Release\ClassificationModules.dll .
-copy %REPOS%\build-kiptool\bin\Release\ClassificationModulesGUI.dll . 
-copy %REPOS%\build-kiptool\bin\Release\ImagingModules.dll .
-copy %REPOS%\build-kiptool\bin\Release\ImagingModulesGUI.dll .
+copy %REPOS%\install\lib\ImagingAlgorithms.dll .
+copy %REPOS%\install\lib\ModuleConfig.dll .
+copy %REPOS%\install\lib\ReaderConfig.dll .
+copy %REPOS%\install\lib\ReaderGUI.dll .
+copy %REPOS%\install\lib\QtAddons.dll .
+copy %REPOS%\install\lib\QtModuleConfigure.dll .
+copy %REPOS%\install\lib\kipl.dll .
+copy %REPOS%\install\lib\ProcessFramework.dll .
+copy %REPOS%\install\lib\AdvancedFilterModules.dll .
+copy %REPOS%\install\lib\AdvancedFilterModulesGUI.dll .
+copy %REPOS%\install\lib\AdvancedFilters.dll .
+copy %REPOS%\install\lib\BaseModules.dll .
+copy %REPOS%\install\lib\BaseModulesGUI.dll .
+copy %REPOS%\install\lib\ClassificationModules.dll .
+copy %REPOS%\install\lib\ClassificationModulesGUI.dll . 
+copy %REPOS%\install\lib\ImagingModules.dll .
+copy %REPOS%\install\lib\ImagingModulesGUI.dll .
 
-copy %REPOS%\imagingsuite\external\lib64\libtiff.dll .
-copy %REPOS%\imagingsuite\external\lib64\libjpeg-62.dll .
-copy %REPOS%\imagingsuite\external\lib64\zlib1.dll .
-copy %REPOS%\imagingsuite\external\lib64\libfftw3-3.dll .
-copy %REPOS%\imagingsuite\external\lib64\libfftw3f-3.dll .
-copy %REPOS%\imagingsuite\external\lib64\liblapack.dll .
-copy %REPOS%\imagingsuite\external\lib64\libgcc_s_seh_64-1.dll .
-copy %REPOS%\imagingsuite\external\lib64\libgcc_s_seh-1.dll .
-copy %REPOS%\imagingsuite\external\lib64\libgfortran_64-3.dll .
-copy %REPOS%\imagingsuite\external\lib64\libquadmath-0.dll .
-copy %REPOS%\imagingsuite\external\lib64\libwinpthread-1.dll .
-copy %REPOS%\imagingsuite\external\lib64\libblas.dll .
-copy %REPOS%\imagingsuite\external\lib64\nexus\NeXus.dll .
-copy %REPOS%\imagingsuite\external\lib64\nexus\NexusCPP.dll .
-copy %REPOS%\imagingsuite\external\lib64\hdf5\hdf5.dll .
-copy %REPOS%\imagingsuite\external\lib64\hdf5\hdf5_cpp.dll .
-copy %REPOS%\imagingsuite\external\lib64\hdf5\zlib.dll .
-copy %REPOS%\imagingsuite\external\lib64\hdf5\szip.dll .
+copy %REPOS%\\install\lib\libtiff.dll .
+copy %REPOS%\install\lib\libjpeg-62.dll .
+copy %REPOS%\install\lib\zlib1.dll .
+copy %REPOS%\install\lib\libfftw3-3.dll .
+copy %REPOS%\install\lib\libfftw3f-3.dll .
+copy %REPOS%\install\lib\liblapack.dll .
+copy %REPOS%\install\lib\libgcc_s_seh_64-1.dll .
+copy %REPOS%\install\lib\libgcc_s_seh-1.dll .
+copy %REPOS%\install\lib\libgfortran_64-3.dll .
+copy %REPOS%\install\lib\libquadmath-0.dll .
+copy %REPOS%\install\lib\libwinpthread-1.dll .
+copy %REPOS%\install\lib\libblas.dll .
+copy %REPOS%\install\lib\nexus\NeXus.dll .
+copy %REPOS%\install\lib\nexus\NexusCPP.dll .
+copy %REPOS%\install\lib\hdf5\hdf5.dll .
+copy %REPOS%\install\lib\hdf5\hdf5_cpp.dll .
+copy %REPOS%\install\lib\hdf5\zlib.dll .
+copy %REPOS%\install\lib\hdf5\szip.dll .
 
-@REM copy %REPOS%\imagingsuite\external\lib64\libgcc_s_seh_64-1.dll .
-@REM copy %REPOS%\imagingsuite\external\lib64\libgcc_s_seh-1.dll .
-@REM copy %REPOS%\imagingsuite\external\lib64\libwinpthread-1.dll .
+@REM copy %REPOS%\install\lib\libgcc_s_seh_64-1.dll .
+@REM copy %REPOS%\install\lib\libgcc_s_seh-1.dll .
+@REM copy %REPOS%\install\lib\libwinpthread-1.dll .
 
-copy %REPOS%\ExternalDependencies\windows\bin\libopenblas.dll .
-copy %REPOS%\ExternalDependencies\windows\bin\libxml2.dll .
-copy %REPOS%\ExternalDependencies\windows\bin\cfitsio.dll .
+@REM copy %REPOS%\install\lib\libopenblas.dll .
+copy %REPOS%\install\lib\libxml2.dll .
+copy %REPOS%\install\lib\cfitsio.dll .
 
 copy %REPOS%\KipTool\applications\kiptool\Resources resources
 cd %QTBINPATH%
